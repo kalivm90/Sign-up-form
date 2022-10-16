8f7cfddcc3c5434ada7d5ccd375733aa5a9be0d9
kalivm90 <kalivm90@gmail.com>
2022-10-16 16:29:30 -0700

    Not very happy with how this turned out. I may revist this when I learned grid and mobile layouts

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,26 @@
 /* TODO */
+document.addEventListener("DOMContentLoaded", () => {
+    let password = document.querySelector("#password");
+    let confirmPassword = document.querySelector("#confirm-password");
+    let submit = document.querySelector("#submit");
+    let error = document.querySelector("#error");
+
+    submit.onclick = e => checkPassword();
+
+    function checkPassword() {
+        console.log("hello")
+        if(password.value != confirmPassword.value) {
+            confirmPassword.setCustomValidity("Passwords Don't Match");
+            confirmPassword.classList.add("addValidation");
+            password.classList.add("addValidation");
+            error.textContent = "*Passwords do not match";
+        } else {
+            confirmPassword.setCustomValidity('');
+            confirmPassword.classList.remove("addValidation");
+            password.classList.remove("addValidation");
+            error.textContent = "";
+        }
+    }
+    // password.onchange = checkPassword;
+    confirmPassword.onkeyup = checkPassword;
+})
